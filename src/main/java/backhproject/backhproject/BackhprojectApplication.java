79bbce8d19c131bf4d1d0dc43c3506861a13147e
seunghyun We <bible091192@gmail.com>
2023-08-04 11:31:21 +0900

    백엔드 프로젝트 틀 생성

---
+++ src/main/java/backhproject/backhproject/BackhprojectApplication.java
@@ -0,0 +1,13 @@
+package backhproject.backhproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackhprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackhprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backhproject/backhproject/BackhprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package backhproject.backhproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackhprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
