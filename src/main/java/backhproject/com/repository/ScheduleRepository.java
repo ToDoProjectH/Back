63b312baf51c36440fd8da5f40f40ba8f3d7618a
seunghyun We <bible091192@gmail.com>
2023-08-24 19:31:35 +0900

    make develop structure
    controller,dto,entity,repository,service폴더 만들고 간단한 클래스 구성
    개발을 시작하는 토대를 만들었다.

---
+++ src/main/java/backhproject/com/repository/ScheduleRepository.java
@@ -0,0 +1,9 @@
+package backhproject.com.repository;
+
+import backhproject.com.entity.ScheduleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Integer> {
+}
---
+++ src/main/java/backhproject/com/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package backhproject.com.repository;
+
+import backhproject.com.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+}
